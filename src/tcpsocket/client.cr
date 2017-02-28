e0c184d0dcad84bb84d8a2a47d7fec5fb22a920e
lgphp <1789666@qq.com>
2017-02-28 23:31:00 +0800

    新增tcp例子

--- src/tcpsocket/client.cr
+++ src/tcpsocket/client.cr
@@ -1,9 +1,10 @@
 require "socket"
 require "json"
+
 module Client
   arr = {retcode: 1, data: [{"username" => "lgphp"}, {"password" => "123445"}]}
   TCPSocket.open "localhost", 9000 do |client|
-    client.puts arr.to_json #如果是对象，一定要转换成json
+    client.puts arr.to_json # 如果是对象，一定要转换成json
     data = client.gets
     puts data
     client.close
--- src/tcpsocket/server.cr
+++ src/tcpsocket/server.cr
@@ -3,16 +3,27 @@ require "socket"
 
 module Server
   TCPServer.open (9000) do |socket|
-    puts "start...."
-    loop do
-      socket.accept? do |client|
-        data = client.gets
-        ret = JSON.parse(data.as(String)) #在接收的时候一定要先用parse将json字符串转成json::any类型
-         puts "retcode=#{ret["retcode"]}" , "data=#{ret["data"]}" , "username=#{ret["data"][0]["username"]}"
-        client.puts "收到数据"
-        client.flush
-        client.close
+    begin
+      puts "start...."
+      loop do
+        socket.accept? do |client|
+          begin
+            data = client.gets
+            ret = JSON.parse(data.as(String)) # 在接收的时候一定要先用parse将json字符串转成json::any类型，用to_s也可以
+            puts "retcode=#{ret["retcode"]}", "data=#{ret["data"]}", "username=#{ret["data"][0]["username"]}"
+            client.puts "收到数据" # 返回给客户端数据  必须用puts方法，如果不用puts方法传送的话，就得有结尾\r\n来结束
+          rescue ex
+            puts "socket服务器发生错误:#{ex.message}"
+          ensure
+            client.flush
+            client.close
+          end
+        end
       end
+    rescue e
+      puts "socket服务器发生错误:#{e.message}"
+    ensure
+      socket.close
     end
   end
 end
